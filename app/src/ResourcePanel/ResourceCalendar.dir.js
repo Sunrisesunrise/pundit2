751348af33532d0798832bb5e07e3341a0e74241
Raffaele Masotti <raffaele.masotti@gmail.com>
2015-06-19 16:20:10 +0200

    ResourceCalendar date validity check

--- app/src/ResourcePanel/ResourceCalendar.dir.js
+++ app/src/ResourcePanel/ResourceCalendar.dir.js
@@ -17,69 +17,124 @@ angular.module('Pundit2.ResourcePanel')
 
             scope.activeFocus = false;
 
-            scope.year = {
-                focus: false,
-                editable: true
+            scope.inputDate = {
+                year: '',
+                month: '',
+                day: '',
+                time: ''
             };
-            scope.month = {
-                focus: false,
-                editable: false
+
+            var isValidField = function(input) {
+                if (typeof(input) === 'undefined') {
+                    return false;
+                }
+                if (input === '') {
+                    return false;
+                }
+                if (isNaN(input)) {
+                    return false;
+                }
+                if (input < 1 || input > 9999) {
+                    return false;
+                }
+
+                return true;
             };
-            scope.day = {
-                focus: false,
-                editable: false
+
+            var updateCalendarView = function(currentDate) {
+                var isValid = currentDate instanceof Date;
+                if (!isValid) {
+                    return;
+                }
+
+                switch (scope.mode) {
+                    case 'month':
+                        if (scope.focus === 'year') {
+                            scope.switchFocus('month');
+                        }
+                        break;
+                    case 'day':
+                        if (scope.focus === 'year') {
+                            scope.switchFocus('month');
+                        } else if (scope.focus === 'month') {
+                            scope.switchFocus('day')
+                        }
+                        break;
+                    case 'time':
+                        if (scope.focus === 'year') {
+                            scope.switchFocus('month');
+                        } else if (scope.focus === 'month') {
+                            scope.switchFocus('day')
+                        } else if (scope.focus === 'day') {
+                            scope.switchFocus('time')
+                        }
+                        break;
+                }
+
             };
-            scope.time = {
-                focus: false,
-                editable: false
+
+            var updateInput = function(currentDate) {
+                var isValid = currentDate instanceof Date;
+                if (!isValid) {
+                    return;
+                }
+
+                var momentDate = moment(currentDate);
+                scope.inputDate = {
+                    year: momentDate.year(),
+                    month: momentDate.month() + 1,
+                    day: momentDate.date(),
+                    time: momentDate.hours() + ':' + momentDate.minutes()
+                };
             };
+
+            scope.updateYear = function() {
+                var currentYear = scope.inputDate.year;
+                if (isValidField(currentYear)) {
+                    var dateWithNewYear = moment(scope.currentDate).year(currentYear);
+                    scope.currentDate = new Date(dateWithNewYear.format());
+                }
+            };
+            scope.updateMonth = function() {
+                var currentMonth = scope.inputDate.month;
+                if (isValidField(currentMonth)) {
+                    var dateWithNewMonth = moment(scope.currentDate).month(currentMonth - 1);
+                    scope.currentDate = new Date(dateWithNewMonth.format());
+                }
+            };
+            scope.updateDay = function() {
+                var currentDay = scope.inputDate.day;
+                if (isValidField(currentDay)) {
+                    var dateWithNewDay = moment(scope.currentDate).date(currentDay);
+                    scope.currentDate = new Date(dateWithNewDay.format());
+                }
+            };
+            // scope.updateTime = function() {
+            //     var dateWithNewYear = moment(scope.currentDate).year(scope.inputDate.year);
+            //     scope.currentDate = new Date(dateWithNewYear.format());
+            // };
 
             scope.switchFocus = function(focus) {
                 scope.focus = focus;
             };
-
-            // scope.model.value = new Date();
-            // scope.model.valid = true;
 
             scope.switchMode = function(mode) {
                 scope.mode = mode;
                 scope.focus = mode;
             };
 
-            scope.$watch('currentDate', function(value) {
-                if (!scope.activeFocus && !isNaN(scope.currentDate)) {
-                    switch (scope.mode) {
-                        case 'month':
-                            if (scope.focus === 'year') {
-                                // if (isValidDate )
-                                scope.switchFocus('month');
-                            }
-                            break;
-                        case 'day':
-                            if (scope.focus === 'year') {
-                                scope.switchFocus('month');
-                            } else if (scope.focus === 'month') {
-                                scope.switchFocus('day')
-                            }
-                            break;
-                        case 'time':
-                            if (scope.focus === 'year') {
-                                // if (isValidDate )
-                                scope.switchFocus('month');
-                            } else if (scope.focus === 'month') {
-                                // if (isValidDate)
-                                scope.switchFocus('day')
-                            } else if (scope.focus === 'day') {
-                                // if (isValidDate)
-                                scope.switchFocus('time')
-                            }
-                            break;
-                    }
-                }
+            scope.$watch('activeFocus', function() {
+                updateInput(scope.currentDate);
             });
 
-
-            // console.log(scope.model);
+            scope.$watch('currentDate', function(value) {
+                if (!scope.activeFocus) {
+                    if (scope.mode !== scope.focus) {
+                        updateCalendarView(value);
+                    }
+                    updateInput(value);
+                }
+            });
         }
     };
 });
