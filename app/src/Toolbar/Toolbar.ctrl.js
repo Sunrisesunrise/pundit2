29d85b31fbf9dbedfd564591e8a5c09e75f7de29
Raffaele Masotti <raffaele.masotti@gmail.com>
2015-07-20 14:29:13 +0200

    Toolbar minor change

--- app/src/Toolbar/Toolbar.ctrl.js
+++ app/src/Toolbar/Toolbar.ctrl.js
@@ -3,7 +3,7 @@
 angular.module('Pundit2.Toolbar')
 
 .controller('ToolbarCtrl', function($scope, $rootScope, $modal, $http, $window, NameSpace, Config, Toolbar, SelectorsManager, Fp3,
-    MyPundit, Dashboard, TripleComposer, AnnotationSidebar, Annomatic, ResourcePanel, NotebookExchange, NotebookCommunication, TemplatesExchange, Analytics, PageHandler, EventDispatcher, Annotation) {
+    MyPundit, Dashboard, TripleComposer, AnnotationSidebar, Annomatic, ResourcePanel, NotebookExchange, NotebookCommunication, TemplatesExchange, Analytics, PageHandler, EventDispatcher) {
 
     $scope.dropdownTemplate = "src/ContextualMenu/dropdown.tmpl.html";
     $scope.dropdownTemplateMyNotebook = "src/Toolbar/myNotebooksDropdown.tmpl.html";
