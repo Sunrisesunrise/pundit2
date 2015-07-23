5fcf1e887aa56d1d778b6df5c995cd3a59221f27
Raffaele Masotti <raffaele.masotti@gmail.com>
2015-07-23 17:22:14 +0200

    Chrome disable icon test

--- app/extensions/chrome/js/background.js
+++ app/extensions/chrome/js/background.js
@@ -2,16 +2,23 @@
  *  Handle the browser action that get's executed if the user pushes
  *  the toolbar icon in Chrome
  */
+
+
 chrome.browserAction.onClicked.addListener(function(tab, url) {
-    
+
+    // chrome.browserAction.setIcon({
+    //     path: {
+    //         19: chrome.extension.getURL("icons/close-icon19.png")
+    //     }
+    // }) 
+
     executeScriptFromURLInTab(tab, 'inject/init.js');
 
-    
+
     executeCSSFromURLInTab(tab, 'inject/css/pundit.css');
-    
+
     executeScriptFromURLInTab(tab, 'inject/extension_conf.js');
     executeScriptFromURLInTab(tab, 'inject/scripts/libs.js');
     executeScriptFromURLInTab(tab, 'inject/scripts/pundit2.js');
 
-    // alert('Client loading ..');
 });
