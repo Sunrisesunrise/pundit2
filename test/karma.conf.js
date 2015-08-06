fa6c5e26048dabd8ee86bd8e1528187dbafb3c58
Raffaele Masotti <raffaele.masotti@gmail.com>
2015-08-06 17:30:46 +0200

    Add ngresize to karma conf

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -14,6 +14,7 @@ module.exports = function(config) {
             'bower_components/angular-moment/angular-moment.min.js',
             'bower_components/angular-datepicker/dist/index.min.js',
             'bower_components/angular-md5/angular-md5.min.js',
+            'bower_components/ng-resize/ngresize.min.js',
 
             // Load the module declaration before the module utilization, or karma
             // will screw up :|
