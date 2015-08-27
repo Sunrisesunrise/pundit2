a47d6c184e8c5faf3fc9aeaebf30aa5b6a871320
Enrico Giusti <klad@Enricos-MacBook-Pro.local>
2015-08-27 15:50:58 +0200

    chacnged conf in chrome extension

--- app/extensions/chrome/inject/extension_conf.js
+++ app/extensions/chrome/inject/extension_conf.js
@@ -1,13 +1,23 @@
 var punditConfig = {
-    //annotationServerBaseURL: " https://staging.punditbrain.netseven.it:8443/annotationserver/",
-    //annotationServerVersion: 'v2',
+    annotationServerBaseURL: " https://staging.punditbrain.netseven.it:8443/annotationserver/",
+    annotationServerVersion: 'v2',
     modules: {
         'Client': {
             active: true
         },
-        'DbpediaSelector': {
+        'Korbo2Selector': {
             active: true,
-            limit: 100
+            instances: [
+                {
+                    container: 'dandeliondbpedia',
+                    infiniteScrolling: false,
+                    label: 'DBPedia',
+                    active: true,
+                    basketID: null,
+                    //url: 'http://dev.korbo2.org/v1',
+                    language: 'en'
+                }
+            ]
         }
     },
     templates: [],
