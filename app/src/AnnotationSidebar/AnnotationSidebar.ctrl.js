96cb1c927ba9463cf82850f296045a2fdd07873d
Raffaele Masotti <raffaele.masotti@gmail.com>
2015-09-03 16:23:07 +0200

    Annotation edit fix

--- app/src/AnnotationSidebar/AnnotationSidebar.ctrl.js
+++ app/src/AnnotationSidebar/AnnotationSidebar.ctrl.js
@@ -527,7 +527,11 @@ angular.module('Pundit2.AnnotationSidebar')
                 annotationId = e.args;
 
             annotationsQueques[eventType].push(annotationId);
-            EventDispatcher.sendEvent('AnnotationSidebar.forceUpdate');
+
+            // TODO: avoid consolidation in save and edit and remove this check
+            if (eventType === 'deleteAnnotation') {
+                EventDispatcher.sendEvent('AnnotationSidebar.forceUpdate');
+            }
         }
     );
 
