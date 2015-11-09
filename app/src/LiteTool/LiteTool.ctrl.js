c27c170f611071d74cab27f03f65a1b7f744dab9
Raffaele Masotti <raffaele.masotti@gmail.com>
2015-11-09 14:22:39 +0100

    LiteTool added some functions

--- app/src/LiteTool/LiteTool.ctrl.js
+++ app/src/LiteTool/LiteTool.ctrl.js
@@ -1,8 +1,29 @@
 angular.module('Pundit2.LiteTool')
 
-.controller('LiteToolCtrl', function($scope, $rootScope, Status, AnnotationSidebar, EventDispatcher, MyPundit) {
+.controller('LiteToolCtrl', function($scope, $rootScope, Status, AnnotationSidebar, EventDispatcher, MyPundit, Analytics) {
 
     $scope.userIsLogged = false;
+    $scope.isAnnotationSidebarExpandedv = false;
+    $scope.userData = {};
+
+    var logout = function() {
+        MyPundit.logout();
+        Analytics.track('buttons', 'click', 'litetool--logout');
+    };
+
+    var editYourProfile = function() {
+        MyPundit.editProfile();
+        Analytics.track('buttons', 'click', 'litetool--editProfile');
+    };
+
+    $scope.userLoggedInDropdown = [{
+        text: 'Edit your profile',
+        click: editYourProfile
+    }, {
+        text: 'Log out',
+        click: logout
+    }];
+    $scope.dropdownTemplate = "src/ContextualMenu/dropdown.tmpl.html";
 
     $scope.annotationsClickHandler = function() {
         AnnotationSidebar.toggle();
@@ -10,9 +31,15 @@ angular.module('Pundit2.LiteTool')
 
     $scope.login = function() {
         MyPundit.login();
+        Analytics.track('buttons', 'click', 'litetool--login');
     };
+
+    EventDispatcher.addListener('AnnotationSidebar.toggle', function(e) {
+        $scope.isAnnotationSidebarExpanded = e.args;
+    });
 
     EventDispatcher.addListener('MyPundit.isUserLogged', function(e) {
         $scope.userIsLogged = e.args;
+        $scope.userData = MyPundit.getUserData();
     });
 });
