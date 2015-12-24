8afc6b9f3ca9dda18df8c215a09a003d8091af39
Raffaele Masotti <raffaele.masotti@gmail.com>
2015-12-24 14:33:26 +0100

    ImageFragmentAnnotatorHelper first linting

--- app/src/Core/ImageFragmentAnnotatorHelper.service.js
+++ app/src/Core/ImageFragmentAnnotatorHelper.service.js
@@ -1,10 +1,11 @@
 /*jshint unused: false*/
 
 angular.module('Pundit2.Core')
+
 // TODO move this service in a better place
 .service('ImageFragmentAnnotatorHelper', function($rootScope, $modal, $window, BaseComponent, EventDispatcher, Config, NameSpace,
     ContextualMenu, XpointersHelper, Item, MyItems, MyPundit, Analytics) {
-    
+
     var imageFragmentHelper = new BaseComponent("ImageFragmentAnnotatorHelper");
 
     // events send by image fragment annotator
@@ -49,23 +50,31 @@ angular.module('Pundit2.Core')
         }
         if (e.data.type === msg.addPolygon) {
             var lastData = e.data;
-            if(!MyPundit.isUserLogged()) {
+            if (!MyPundit.isUserLogged()) {
                 var der = EventDispatcher.addListener('Consolidation.consolidateAll', function() {
                     var item = imageFragmentHelper.createItemFromPolygon(lastData.poly);
-                    MyItems.addItem(item).then(function(){
-                        childWindow.postMessage({type:'pundit-add-to-my-items-success'},'*');
-                    }, function(){
-                        childWindow.postMessage({type:'pundit-add-to-my-items-err'},'*');
+                    MyItems.addItem(item).then(function() {
+                        childWindow.postMessage({
+                            type: 'pundit-add-to-my-items-success'
+                        }, '*');
+                    }, function() {
+                        childWindow.postMessage({
+                            type: 'pundit-add-to-my-items-err'
+                        }, '*');
                     });
                     EventDispatcher.removeListener(der);
                 });
                 MyPundit.login();
             } else {
                 var item = imageFragmentHelper.createItemFromPolygon(lastData.poly);
-                MyItems.addItem(item).then(function(){
-                    childWindow.postMessage({type:'pundit-add-to-my-items-success'},'*');
-                }, function(){
-                    childWindow.postMessage({type:'pundit-add-to-my-items-err'},'*');
+                MyItems.addItem(item).then(function() {
+                    childWindow.postMessage({
+                        type: 'pundit-add-to-my-items-success'
+                    }, '*');
+                }, function() {
+                    childWindow.postMessage({
+                        type: 'pundit-add-to-my-items-err'
+                    }, '*');
                 });
             }
         }
@@ -78,9 +87,9 @@ angular.module('Pundit2.Core')
 
         // add iframe to the page
         if ((newSrc.substring(0, 7) === "http://") || (newSrc.substring(0, 8) === "https://")) {
-            angular.element("[data-ng-app='Pundit2']").append(template1+newSrc+template2);
+            angular.element("[data-ng-app='Pundit2']").append(template1 + newSrc + template2);
         } else {
-            angular.element("[data-ng-app='Pundit2']").append(template1+"http://"+newSrc+template2);
+            angular.element("[data-ng-app='Pundit2']").append(template1 + "http://" + newSrc + template2);
         }
         // disable page scroll
         overflow = angular.element('body').css('overflow');
@@ -112,7 +121,7 @@ angular.module('Pundit2.Core')
             image: imgItem.image,
             pageContext: XpointersHelper.getSafePageContext(),
             polygon: angular.copy(poly.points),
-            polygonUri: NameSpace.selectors.baseURI + 'polygon/' + (new Date().getTime()+1),
+            polygonUri: NameSpace.selectors.baseURI + 'polygon/' + (new Date().getTime() + 1),
             parentItemXP: imgItem.xpointer
         };
 
@@ -121,21 +130,21 @@ angular.module('Pundit2.Core')
 
     imageFragmentHelper.drawPolygonOverImage = function(points, img) {
 
-        if(typeof(img) === 'undefined' || typeof(points) === 'undefined' || !img.is('img, svg')){
+        if (typeof(img) === 'undefined' || typeof(points) === 'undefined' || !img.is('img, svg')) {
             return false;
         }
 
         var w = img.width(),
             h = img.height(),
             i;
-        
+
         // TODO padding?
 
-        var ofx = ((img.outerWidth(true)) - w)/2,
-            ofy = (img.outerHeight(true) - h)/2;
+        var ofx = ((img.outerWidth(true)) - w) / 2,
+            ofy = (img.outerHeight(true) - h) / 2;
 
         var html = '<span class="pnd-cons-svg"></span>';
-        var htmlSVG = '<svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.0" class="pnd-polygon-layer" width='+w+' height='+h+'></svg>';
+        var htmlSVG = '<svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.0" class="pnd-polygon-layer" width=' + w + ' height=' + h + '></svg>';
 
         var container = angular.element(html).insertBefore(img).css({
             'position': 'absolute',
@@ -147,30 +156,33 @@ angular.module('Pundit2.Core')
         var svg = angular.element(htmlSVG).appendTo(container);
 
         var normPoints = [];
-        for (i=0; i<points.length; i++) {
-            normPoints.push({x: points[i].x*w, y: points[i].y*h});
+        for (i = 0; i < points.length; i++) {
+            normPoints.push({
+                x: points[i].x * w,
+                y: points[i].y * h
+            });
         }
 
         var lines = [];
-        for (i=0; i<normPoints.length; i++) {
+        for (i = 0; i < normPoints.length; i++) {
             // last point is connected to the first point
             var p, np;
-            if (i === normPoints.length-1) {
+            if (i === normPoints.length - 1) {
                 p = normPoints[i];
                 np = normPoints[0];
             } else {
                 p = normPoints[i];
-                np = normPoints[i+1];
+                np = normPoints[i + 1];
             }
-            lines.push('<line x1='+p.x+' y1='+p.y+' x2='+np.x+' y2='+np.y+' ></line>');
+            lines.push('<line x1=' + p.x + ' y1=' + p.y + ' x2=' + np.x + ' y2=' + np.y + ' ></line>');
         }
 
         var filling = '<polygon points="';
-        for (i=0; i<normPoints.length; i++) {
-            filling += normPoints[i].x+','+normPoints[i].y+' ';
+        for (i = 0; i < normPoints.length; i++) {
+            filling += normPoints[i].x + ',' + normPoints[i].y + ' ';
         }
         filling += '"></polygon>';
-        
+
         // add line and filling polygon inside svg element
         svg.append(lines);
         svg.find('line').css({
