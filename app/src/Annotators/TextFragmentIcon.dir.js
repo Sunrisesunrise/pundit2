d6b5f0e97fc904642e396666ee1c594f0da1efe4
Raffaele Masotti <raffaele.masotti@gmail.com>
2015-12-24 14:31:14 +0100

    TextFragmentIcon dir linting

--- app/src/Annotators/TextFragmentIcon.dir.js
+++ app/src/Annotators/TextFragmentIcon.dir.js
@@ -12,48 +12,47 @@ angular.module('Pundit2.Annotators')
         replace: true,
         link: function(scope, element /*, attrs */ ) {
 
-                // TODO: move this to its own controller !
+            // TODO: move this to its own controller !
+
+            scope.textFragmentIconClass = XpointersHelper.options.textFragmentIconClass;
+
+            // Will use the icon to calculate this fragment height with respect to
+            // the document
+            scope.element = element;
+            TextFragmentAnnotator.addFragmentIcon(scope);
+
+            // To see what kind of item is it, check which container it belongs to
+            var piContainer = Config.modules.PageItemsContainer.container,
+                miContainer = Config.modules.MyItemsContainer.container;
+
+            if (ItemsExchange.isItemInContainer(scope.item, miContainer) &&
+                !ItemsExchange.isItemInContainer(scope.item, piContainer)) {
+
+                scope.iconClass = TextFragmentAnnotator.options.myItemsIconClass;
+            } else {
+                scope.iconClass = TextFragmentAnnotator.options.annotationIconClass;
+            }
 
 
-                scope.textFragmentIconClass = XpointersHelper.options.textFragmentIconClass;
+            // TODO: move this to its own controller?
+            scope.mouseoverHandler = function() {
+                TextFragmentAnnotator.highlightById(scope.fragment);
+            };
 
-                // Will use the icon to calculate this fragment height with respect to
-                // the document
-                scope.element = element;
-                TextFragmentAnnotator.addFragmentIcon(scope);
+            scope.mouseoutHandler = function() {
+                TextFragmentAnnotator.clearHighlightById(scope.fragment);
+            };
 
-                // To see what kind of item is it, check which container it belongs to
-                var piContainer = Config.modules.PageItemsContainer.container,
-                    miContainer = Config.modules.MyItemsContainer.container;
-
-                if (ItemsExchange.isItemInContainer(scope.item, miContainer) &&
-                    !ItemsExchange.isItemInContainer(scope.item, piContainer)) {
-
-                    scope.iconClass = TextFragmentAnnotator.options.myItemsIconClass;
-                } else {
-                    scope.iconClass = TextFragmentAnnotator.options.annotationIconClass;
+            scope.clickHandler = function(event) {
+                if (Config.modules.Client.active) {
+                    ContextualMenu.show(event.pageX, event.pageY, scope.item, TextFragmentAnnotator.options.cMenuType);
+                } else if (Config.modules.SimplifiedClient.active) {
+                    ItemPopover.show(scope.element, scope.item);
                 }
+                event.stopPropagation();
+                event.preventDefault();
+            };
 
-
-                // TODO: move this to its own controller?
-                scope.mouseoverHandler = function() {
-                    TextFragmentAnnotator.highlightById(scope.fragment);
-                };
-
-                scope.mouseoutHandler = function() {
-                    TextFragmentAnnotator.clearHighlightById(scope.fragment);
-                };
-
-                scope.clickHandler = function(event) {
-                    if (Config.modules.Client.active) {
-                        ContextualMenu.show(event.pageX, event.pageY, scope.item, TextFragmentAnnotator.options.cMenuType);
-                    } else if (Config.modules.SimplifiedClient.active) {
-                        ItemPopover.show(scope.element, scope.item);
-                    }
-                    event.stopPropagation();
-                    event.preventDefault();
-                };
-
-            } // link()
+        } // link()
     };
 });
